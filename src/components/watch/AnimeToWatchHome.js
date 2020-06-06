24ba98b69ad53cc423dcf001b4720eeef39fcd24
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-06-06 01:31:37 -0500

    nightly progress

--- src/components/watch/AnimeToWatchHome.js
+++ src/components/watch/AnimeToWatchHome.js
@@ -1,36 +1,44 @@
-import React, { useEffect, useContext } from "react";
-import { UserContext } from "../../contexts/contexts";
-import Axios from "axios";
-import LoadingSquare from "./LoadingSquare";
-import AddNewShowToWatch from "./AddNewShowToWatch";
+import React,{useEffect,useContext} from 'react'
+import {UserContext} from '../../contexts/contexts';
+import Axios from 'axios'
+import LoadingSquare from './LoadingSquare'
+import { Link } from "react-router-dom";
+import { AiFillPlusCircle } from "react-icons/ai";
 export default function AnimeToWatchHome() {
-  const { state, dispatch } = useContext(UserContext);
+    const {state,dispatch}= useContext(UserContext)
 
-  useEffect(() => {
-    Axios.get(
-      `https://anime-list-api.herokuapp.com/watch/${state.user.user_id}`
-    )
-      .then((res) => {
-        console.log(res.data);
-        dispatch({ type: "myWatchList", payload: res.data });
-      })
-      .catch((err) => {});
-  }, []);
-  if (state.watchList === null) {
+
+    useEffect(() => {
+    Axios.get(`https://anime-list-api.herokuapp.com/watch/${state.user.user_id}`)
+    .then(res => {
+        console.log(res.data)
+        dispatch({type:"myWatchList",payload:res.data})
+    })
+    .catch(err => {
+
+    })
+    }, [])
+    if(state.watchList === null){
+        return(
+            <div>
+                Loading <LoadingSquare />
+            </div>
+        )
+    }else if(state.watchList.length === 0){
+        return (
+            <div>
+                Please Add A Show
+                <div>
+                <Link to="/watchadd">
+                    <AiFillPlusCircle />
+                </Link>
+                </div>
+            </div>
+        )
+    }
     return (
-      <div>
-        Loading <LoadingSquare />
-      </div>
-    );
-  } else if (state.watchList.length === 0) {
-    return (
-      <div>
-        Please Add A Show
         <div>
-          <AddNewShowToWatch />
+            
         </div>
-      </div>
-    );
-  }
-  return <div></div>;
+    )
 }
